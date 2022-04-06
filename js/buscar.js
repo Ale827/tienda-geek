7b9c5bfb9c125a89caeac0d799e5682a94c4e531
Ale827 <alejandrosistem@hotmail.com>
2022-04-06 13:27:04 -0300

    primer maquetado del challenge de frontend Alura-Geek

---
+++ js/buscar.js
@@ -0,0 +1,21 @@
+var buscar = document.getElementById('search');
+
+ buscar.addEventListener('focus',function(){
+    document.getElementsByName('buscar')[0].placeholder='¿Que desea buscar?';
+
+
+})
+ buscar.addEventListener('blur',function(){
+    document.getElementsByName('buscar')[0].placeholder='Click me. . .';
+
+})
+
+//otra opcion para obtener el placeholder
+
+// let input = getElementById( 'inputID' );
+
+// // Obtener el valor del placeholder
+// let placeholder = input.placeholder;
+
+// // Establecer un nuevo valor
+// input.placeholder = "Aquí el nuevo valor para el placeholder";
